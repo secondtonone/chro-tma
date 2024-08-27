f4c728b58473f1ecd65abe6bf5f5e0fdcaa49031
Maxim Kuznetsov <fixer40@bk.ru>
2024-08-27 19:45:59 +0700

    Fix types for build

--- src/entities/query/lib/validators.ts
+++ src/entities/query/lib/validators.ts
@@ -1,6 +1,7 @@
 import { Contract } from '@withease/contracts';
 
 export const stringNotEmpty = (error = ''): Contract<unknown, boolean> => ({
+  // @ts-expect-error Incorrect library type
   isData: (data: string) => data.length > 0,
   getErrorMessages: () => [error],
 });
